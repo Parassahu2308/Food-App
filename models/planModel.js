95ce3c956f2593b43de098220a382feefb5732a6
Parassahu2308 <sahujuparas786@gmail.com>
2022-12-04 09:20:56 +0530

    :fire:plan schema created

---
+++ models/planModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { db_link } = require("../secret");
+const { v4, uuidv4 } = require("uuid");
+
+mongoose
+  .connect(db_link)
+  .then(function (db) {
+    console.log(" plan db is connected");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+const planSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: [true, `plan name exist`],
+    maxLength: [20, `plan name should not exceed 20 characters`],
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: [true, `price not entered`],
+  },
+  discount: {
+    type: Number,
+    validate: [
+      function () {
+        return this.discount < 100;
+      },
+      `discount cannot be 100%`,
+    ],
+  },
+  averageRating: {
+    type: Number,
+  },
+});
+
+const planModel = mongoose.model("planModel", planSchema);
+module.exports = planModel;
+
+(async function createPlan() {
+  let plan = {
+    name: "SuperChineseFood",
+    price: 10000,
+    discount: 100,
+    averageRating: 4,
+    duration: 5,
+  };
+  let data = await planModel.create(plan);
+  console.log(data);
+})();
